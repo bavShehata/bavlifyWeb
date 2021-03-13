b2c03e4fbede80dd55fc479c0b4735e3f25db3ea
bavshehata <bavshehata@gmail.com>
2021-03-13 23:05:38 +0200

    From mongo to JSONServer for portfolios

--- controllers/portfolioControllers.js
+++ controllers/portfolioControllers.js
@@ -1,13 +1,18 @@
-const mongoose = require("mongoose");
-const Portfolio = require("../models/portfolioModel");
-
+const mongoose = require('mongoose');
+const Portfolio = require('../models/portfolioModel');
+const uri = 'http://localhost:3000/portfolios';
+const axios = require('axios');
 module.exports = {
   //Show index page
   getIndex: async (req, res) => {
     try {
-      const portfolios = await Portfolio.find();
-      console.log("Number of portfolios: ", portfolios.length);
-      res.render("mainViews/index.ejs", { portfolios, contact: 0 });
+      // The Mongo way
+      // const portfolios = await Portfolio.find();
+      // The JSON-server way
+      const result = await axios.get(uri);
+      const portfolios = result.data;
+      console.log('Number of portfolios: ', portfolios.length);
+      res.render('mainViews/index.ejs', { portfolios, contact: 0 });
     } catch (e) {
       console.log("Couldn't show index page\n", e);
     }
@@ -17,8 +22,8 @@ module.exports = {
     try {
       // const portfolios = await Portfolio.find();
       portfolios = [];
-      console.log("Number of portfolios: ", portfolios.length);
-      res.render("mainViews/index.ejs", { portfolios });
+      console.log('Number of portfolios: ', portfolios.length);
+      res.render('mainViews/index.ejs', { portfolios });
     } catch (e) {
       console.log("Couldn't show index page\n", e);
     }
@@ -27,7 +32,7 @@ module.exports = {
   deletePortfolios: async (req, res) => {
     try {
       await Portfolio.deleteMany();
-      console.log("All portfolios deleted successfully");
+      console.log('All portfolios deleted successfully');
     } catch (e) {
       console.log("Couldn't delete all portfolios\n", e);
     }
@@ -35,7 +40,7 @@ module.exports = {
   // Portfolio form page
   portfolioForm: async (req, res) => {
     try {
-      res.render("mainViews/portfolioOperations.ejs");
+      res.render('mainViews/portfolioOperations.ejs');
     } catch (e) {
       console.log("Couldn't delete all portfolios\n", e);
     }
@@ -44,14 +49,14 @@ module.exports = {
   addPortfolio: async (req, res) => {
     const { title, desktopURL, mobileURL, websiteURL } = req.body;
     try {
-      console.log("obj");
+      console.log('obj');
       const portfolio = await Portfolio.create({
         title,
         desktopURL,
         mobileURL,
         websiteURL,
       });
-      console.log("Portfolio created successfully: ", title);
+      console.log('Portfolio created successfully: ', title);
     } catch (e) {
       console.log("Couldn't create portfolio\n", e);
     }
@@ -61,7 +66,7 @@ module.exports = {
     const { mobileURL } = req.body;
     try {
       const deletedPorfolio = await Portfolio.findOneAndDelete({ mobileURL });
-      console.log("Portfolio deleted successfully", deletePortfolio.title);
+      console.log('Portfolio deleted successfully', deletePortfolio.title);
     } catch (e) {
       console.log("Couldn't delete portfolio\n", e);
     }
--- server.js
+++ server.js
@@ -1,58 +1,61 @@
 //Environment variables
-require("dotenv/config");
+require('dotenv/config');
 
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const favicon = require("serve-favicon");
-const path = require("path");
-const axios = require("axios");
-const { body, validationResult } = require("express-validator");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const favicon = require('serve-favicon');
+const path = require('path');
+const axios = require('axios');
+const { body, validationResult } = require('express-validator');
 
 //Connect to database
 const app = express();
 
 const dbURI = `mongodb+srv://${process.env.dbUser}:${process.env.dbPass}@${process.env.clusterName}.jgkgp.mongodb.net/${process.env.dbName}?retryWrites=true&w=majority`;
-mongoose
-  .connect(dbURI, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-  })
-  .then(
-    app.listen(8001, () => {
-      console.log("Listening on port 8001");
-      let width = 360;
-      let large = width * 0.56;
-      let medium = large * 0.674;
-      let small = large * 0.52;
-      console.log(large, medium, small);
-    })
-  )
-  .catch((error) => console.log("Couldn't connect to database\n", error));
+// mongoose
+//   .connect(dbURI, {
+//     useUnifiedTopology: true,
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//   })
+//   .then(
+//     app.listen(8001, () => {
+//       console.log("Listening on port 8001");
+//       let width = 360;
+//       let large = width * 0.56;
+//       let medium = large * 0.674;
+//       let small = large * 0.52;
+//       console.log(large, medium, small);
+//     })
+//   )
+//   .catch((error) => console.log("Couldn't connect to database\n", error));
 
+app.listen(8001, () => {
+  console.log('Listening on port 8001');
+});
 // middleware & static files
-app.use(favicon(path.join(__dirname, "public/assets/hero", "logo.png")));
-app.use("/scripts", express.static(__dirname + "/node_modules/mailcheck/src/"));
-app.use(express.static("public"));
+app.use(favicon(path.join(__dirname, 'public/assets/hero', 'logo.png')));
+app.use('/scripts', express.static(__dirname + '/node_modules/mailcheck/src/'));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // register view engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 //Routes
-const formControllers = require("./controllers/formControllers");
-const portfolioControllers = require("./controllers/portfolioControllers");
+const formControllers = require('./controllers/formControllers');
+const portfolioControllers = require('./controllers/portfolioControllers');
 
 //User Routes
-app.get("/", portfolioControllers.getIndex);
+app.get('/', portfolioControllers.getIndex);
 app.post(
-  "/",
+  '/',
   formControllers.validateContact,
   formControllers.sanitizeContact,
   formControllers.postContact
 );
-app.get("/portfolio", portfolioControllers.portfolioForm);
-app.post("/portfolio", portfolioControllers.addPortfolio);
-app.get("/contact/success", formControllers.getContactSuccess);
+app.get('/portfolio', portfolioControllers.portfolioForm);
+app.post('/portfolio', portfolioControllers.addPortfolio);
+app.get('/contact/success', formControllers.getContactSuccess);
