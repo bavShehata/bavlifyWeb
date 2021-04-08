7d0d6b613822604fb23abdacdda04f38f9410dc3
bavshehata <bavshehata@gmail.com>
2021-04-08 20:50:50 +0200

    dynamic portfolio resizing

--- public/app.js
+++ public/app.js
@@ -65,6 +65,28 @@ let usedDevice;
 window.onload = function () {
   // Wait for everything to load so that we can get the offsetHeight and width accurately, then run the function of slideShowing
   var desktopVidHeight, mobileVidHeight, desktopVidWidth, mobileVidWidth;
+  //listen for window resize event
+  window.addEventListener("resize", function (event) {
+    desktopVidHeight = slides[5].querySelector("video").offsetHeight; // aspect ratio of 1.73:1
+    mobileVidHeight = slides[1].querySelector("video").offsetHeight; // aspect ratio of 11:20
+    desktopVidWidth = slides[5].querySelector("video").offsetWidth; // aspect ratio of 1.73:1
+    mobileVidWidth = slides[1].querySelector("video").offsetWidth; // aspect ratio of 11:20
+    usedDevice = getComputedStyle(
+      document.querySelector("#portfolio .container")
+    ).getPropertyValue("--device");
+    for (let n = 0; n < 6; n++) {
+      const desktopVid = slides[n].querySelector(".desktop video");
+      const img = slides[n].querySelector("img");
+      if (desktopVid) {
+        img.style.width = mobileVidWidth;
+        img.style.height = mobileVidHeight;
+      } else {
+        const mobileVid = slides[n].querySelector(".mobile video");
+        img.style.width = desktopVidWidth;
+        img.style.height = desktopVidHeight;
+      }
+    }
+  });
   desktopVidHeight = slides[5].querySelector("video").offsetHeight; // aspect ratio of 1.73:1
   mobileVidHeight = slides[1].querySelector("video").offsetHeight; // aspect ratio of 11:20
   desktopVidWidth = slides[5].querySelector("video").offsetWidth; // aspect ratio of 1.73:1
@@ -147,20 +169,16 @@ const throttleFunction = (func, delay) => {
 };
 // Next/previous controls
 function plusSlides(n) {
-  console.log("N in the plusSides function: ", n);
-  console.log("slideIndex in the plusSides function: ", slideIndex);
   window.clearTimeout(isBrowsing);
   showSlide((slideIndex += n));
 }
 
 function showSlide(n) {
   const maxNum = slides.length;
-  if (n == -1){
+  if (n == -1) {
     n = 5;
   }
   n = Math.abs(n % maxNum);
-  console.log("N in the showSlide function: ", n);
-  console.log("slideIndex in the showSlide function: ", slideIndex);
   slideIndex = n;
   // portfolioContainer.scroll({
   //   left: n * screenWidth,
@@ -179,20 +197,20 @@ function showSlide(n) {
   //       console.log("Error playing video\n", e);
   //     });
   slides[n]
-  .querySelector("video")
-  .play()
-  .catch(function (e) {
-    console.log("Error playing video\n", e);
-  });
-  // If you are on desktop, the appear class will get toggled 
+    .querySelector("video")
+    .play()
+    .catch(function (e) {
+      console.log("Error playing video\n", e);
+    });
+  // If you are on desktop, the appear class will get toggled
   // if (usedDevice == "desktop") {
-    // Cycle from 0 to number of slides
-    for (i = 0; i < slides.length; i++) {
-      slides[i].classList.remove("appear");
-    }
+  // Cycle from 0 to number of slides
+  for (i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("appear");
+  }
 
-    slides[n].classList.add("appear");
-  // } 
+  slides[n].classList.add("appear");
+  // }
 
   // Auto browse every 4.5 seconds
   isBrowsing = setTimeout(function () {
