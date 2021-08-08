ad42d266383e0e1046b9f3f06a6b9fa73fadbc40
bavshehata <bavshehata@gmail.com>
2021-08-08 11:06:34 +0200

    polishing website

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 //Environment variables
-require('dotenv/config');
+require("dotenv/config");
 
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const favicon = require('serve-favicon');
-const path = require('path');
-const axios = require('axios');
-const { body, validationResult } = require('express-validator');
-const cors = require('cors');
-var enforce = require('express-sslify');
-
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const favicon = require("serve-favicon");
+const path = require("path");
+const axios = require("axios");
+const { body, validationResult } = require("express-validator");
+const cors = require("cors");
+var enforce = require("express-sslify");
 
 //Connect to database, just for the contacts, as portfolios are in JSON Server now
 const app = express();
@@ -19,17 +18,15 @@ const app = express();
 var dbURI = `mongodb+srv://${process.env.dbUser}:${process.env.dbPass}@${process.env.clusterName}.jgkgp.mongodb.net/${process.env.dbName}?retryWrites=true&w=majority`;
 //2.12 or later
 dbURI = `mongodb://${process.env.dbUser}:${process.env.dbPass}@${process.env.clusterName}-shard-00-00.jgkgp.mongodb.net:27017,${process.env.clusterName}-shard-00-01.jgkgp.mongodb.net:27017,${process.env.clusterName}-shard-00-02.jgkgp.mongodb.net:27017/${process.env.dbName}?ssl=true&replicaSet=atlas-wdji3c-shard-0&authSource=admin&retryWrites=true&w=majority`;
-console.log('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n');
-console.log(
-  '------------------Server initiated-------------------\n',
-);
+console.log("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+console.log("------------------Server initiated-------------------\n");
 mongoose
   .connect(dbURI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useCreateIndex: true,
   })
-  .then(console.log('Connected to database\n Be Happy!!\n'))
+  .then(console.log("Connected to database\n Be Happy!!\n"))
   .catch((error) => console.log("Couldn't connect to database\n", error));
 
 // with JSON Server, we can listen to the port without having to connect to db.
@@ -52,28 +49,30 @@ app.listen(port, () => {
   // console.log(`Listening on port ${port}`);
 });
 // middleware & static files
-app.use(favicon(path.join(__dirname, 'public/assets/hero', 'logo.png')));
+app.use(favicon(path.join(__dirname, "public/assets/hero", "logo.png")));
 app.use(cors());
-app.use('/scripts', express.static(__dirname + '/node_modules/mailcheck/src/'));
-app.use(express.static('public'));
+app.use("/scripts", express.static(__dirname + "/node_modules/mailcheck/src/"));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(enforce.HTTPS({ trustProtoHeader: true,trustXForwardedHostHeader: true }))
+app.use(
+  enforce.HTTPS({ trustProtoHeader: true, trustXForwardedHostHeader: true })
+);
 // register view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 //Routes
-const formControllers = require('./controllers/formControllers');
-const portfolioControllers = require('./controllers/portfolioControllers');
+const formControllers = require("./controllers/formControllers");
+const portfolioControllers = require("./controllers/portfolioControllers");
 
 //User Routes
-app.get('/', portfolioControllers.getIndex);
+app.get("/", portfolioControllers.getIndex);
 app.post(
-  '/',
+  "/",
   formControllers.validateContact,
   formControllers.sanitizeContact,
   formControllers.postContact
 );
 app.use((req, res) => {
-  res.render('mainViews/404.ejs', { title: 404 });
+  res.render("mainViews/404.ejs", { title: 404 });
 });
